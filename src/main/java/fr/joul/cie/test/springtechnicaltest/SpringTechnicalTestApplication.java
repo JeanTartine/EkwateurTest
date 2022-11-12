cee83404ed3d3105502eba7cfb006ed9cad2158f
Maxime Rocher <maxirocher@hotmail.fr>
2022-11-12 11:51:57 +0100

    Added json files with correct indentation in ressources and pom dependency for logging

--- src/main/java/fr/joul/cie/test/springtechnicaltest/SpringTechnicalTestApplication.java
+++ src/main/java/fr/joul/cie/test/springtechnicaltest/SpringTechnicalTestApplication.java
@@ -3,17 +3,22 @@ package fr.joul.cie.test.springtechnicaltest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.*;
 
 @SpringBootApplication
 public class SpringTechnicalTestApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	public static final Logger LOG = LoggerFactory.getLogger(SpringTechnicalTestApplication.class);
 
+	public static void main(String[] args) {
+		LOG.info("Starting application");
 		SpringApplication.run(SpringTechnicalTestApplication.class, args);
+		LOG.info("Ending application");
 	}
 
 	@Override
 	public void run(final String... args) throws Exception {
+
 		LOG.info("EXECUTING : command line runner");
 
 		for (int i = 0; i < args.length; ++i) {
