36fe33d75303cfaca3b9f7f53c6b809d36852792
Maxime Rocher <maxirocher@hotmail.fr>
2022-11-12 11:21:59 +0100

    Deleting one repertory level

--- src/main/java/fr/joul/cie/test/springtechnicaltest/SpringTechnicalTestApplication.java
+++ src/main/java/fr/joul/cie/test/springtechnicaltest/SpringTechnicalTestApplication.java
@@ -8,11 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringTechnicalTestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringTechnicalTestApplication.class, args);
 	}
 
 	@Override
 	public void run(final String... args) throws Exception {
-		//Start here !
+		LOG.info("EXECUTING : command line runner");
+
+		for (int i = 0; i < args.length; ++i) {
+			LOG.info("args[{}]: {}", i, args[i]);
+		}
 	}
 }
