6da37f0bbf88c269d25c34566eb7d84ee24fdd5d
Maxime Rocher <maxirocher@hotmail.fr>
2022-11-12 10:22:44 +0100

    Added newer java version starter

--- spring-technical-test-java-17/src/main/java/fr/joul/cie/test/springtechnicaltest/SpringTechnicalTestApplication.java
+++ spring-technical-test-java-17/src/main/java/fr/joul/cie/test/springtechnicaltest/SpringTechnicalTestApplication.java
@@ -1,12 +1,10 @@
 package fr.joul.cie.test.springtechnicaltest;
 
-import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Log
 public class SpringTechnicalTestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
