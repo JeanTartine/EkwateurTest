3383ff65dee8fc19d811813af79826d351b57c4a
Maxime Rocher <maxirocher@hotmail.fr>
2022-11-13 14:12:40 +0100

    Implemented Response Entity in the controller return function

--- src/main/java/fr/joul/cie/test/springtechnicaltest/controller/DiscountController.java
+++ src/main/java/fr/joul/cie/test/springtechnicaltest/controller/DiscountController.java
@@ -7,6 +7,8 @@ import fr.joul.cie.test.springtechnicaltest.service.DiscountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,13 +35,17 @@ public class DiscountController {
     //This function return all the available discounts for a promotion code, if the code is valid,
     //Result is put in a json file into src/main/folder
     @GetMapping("/{codePromo}")
-    public void getAllDiscountFromCode(@RequestParam String codePromo) throws IOException {
+    public ResponseEntity<List<DiscountDto>> getAllDiscountFromCode(@RequestParam String codePromo) throws IOException {
         LOG.info("Discount code used : {}", codePromo);
 
         if (!discountCodeService.discountCodeExists(codePromo)) {
             LOG.error("The discount code does not exist, sorry!");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .build();
         } else if (!discountCodeService.discountCodeValid(codePromo)){
             LOG.error("Sorry, the discount has expired...");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .build();
         } else {
             LOG.info("Code Found");
             List<DiscountDto> discountDtoList = discountService.findAllDiscountByDiscountCode(codePromo);
@@ -47,8 +53,12 @@ public class DiscountController {
             if (discountDtoList != null) {
                 LOG.info("Discounts Found");
                 discountConverter.convertDiscountObjectToJson(discountDtoList);
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(discountDtoList);
             } else {
                 LOG.error("Sorry, no discounts are available for your code...");
+                return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                        .build();
             }
         }
     }
